582452b7c52b77a0c7e10ba67b1f62052ec43f07
Try Ajitiono <aji@tetrate.io>
2021-09-18 13:56:29 +0700

    fix
    Signed-off-by: Try Ajitiono <aji@tetrate.io>

--- examples/next/pages/index.tsx
+++ examples/next/pages/index.tsx
@@ -1,14 +1,27 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
-import styles from '../styles/Home.module.css';
-
 import { calculate } from 'count-up-down';
+import { useEffect, useState } from 'react';
+
+import styles from '../styles/Home.module.css';
 
 const ONE_HOUR_AGO = new Date(new Date().getTime() - 3600 * 1000);
 
 const Home: NextPage = () => {
-  console.log(calculate(ONE_HOUR_AGO, new Date()));
+  const [state, setState] = useState(calculate(ONE_HOUR_AGO, new Date()));
+
+  useEffect(() => {
+    function tick() {
+      setState(calculate(ONE_HOUR_AGO, new Date()));
+    }
+
+    const interval = setInterval(tick, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
 
   return (
     <div className={styles.container}>
@@ -29,33 +42,30 @@ const Home: NextPage = () => {
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <div className={styles.card}>
+            <div className={styles.value}>{state.result.years}</div>
+            <div className={styles.unit}>years</div>
+          </div>
+          <div className={styles.card}>
+            <div className={styles.value}>{state.result.months}</div>
+            <div className={styles.unit}>months</div>
+          </div>
+          <div className={styles.card}>
+            <div className={styles.value}>{state.result.days}</div>
+            <div className={styles.unit}>days</div>
+          </div>
+          <div className={styles.card}>
+            <div className={styles.value}>{state.result.hours}</div>
+            <div className={styles.unit}>hours</div>
+          </div>
+          <div className={styles.card}>
+            <div className={styles.value}>{state.result.minutes}</div>
+            <div className={styles.unit}>minutes</div>
+          </div>
+          <div className={styles.card}>
+            <div className={styles.value}>{state.result.seconds}</div>
+            <div className={styles.unit}>seconds</div>
+          </div>
         </div>
       </main>
 
