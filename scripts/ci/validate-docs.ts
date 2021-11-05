c4beaaeab7458bc13e9f920781402414a3994d7d
Try Ajitiono <ballinst@gmail.com>
2021-11-05 10:21:06 +0700

    check
    Signed-off-by: Try Ajitiono <ballinst@gmail.com>

--- scripts/ci/validate-docs.ts
+++ scripts/ci/validate-docs.ts
@@ -2,7 +2,7 @@ import execa from 'execa';
 
 (async () => {
   await execa('yarn', ['generate:docs']);
-  const { stdout } = await execa('git', ['status']);
+  const { stdout } = await execa('git', ['diff --name-only']);
 
   console.log(stdout);
 })();
