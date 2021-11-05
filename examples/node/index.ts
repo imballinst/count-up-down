f236f35916a3de20700e26a13f46f1f9b4071e13
Try Ajitiono <ballinst@gmail.com>
2021-11-05 11:02:12 +0700

    update README.md and comment
    Signed-off-by: Try Ajitiono <ballinst@gmail.com>

--- examples/node/index.ts
+++ examples/node/index.ts
@@ -2,4 +2,8 @@ import { calculate } from 'count-up-down';
 
 const ONE_HOUR_AGO = new Date(new Date().getTime() - 3600 * 1000);
 
+// {
+//   result: { days: 0, hours: 1, minutes: 0, months: 0, seconds: 0, years: 0 },
+//   type: 'countdown'
+// }
 console.log(calculate(new Date(), ONE_HOUR_AGO));
