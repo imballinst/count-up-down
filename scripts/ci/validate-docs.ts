d032fc06e2434d957968b5e9109ed31fb0c44714
Try Ajitiono <ballinst@gmail.com>
2021-11-05 10:42:22 +0700

    ok
    Signed-off-by: Try Ajitiono <ballinst@gmail.com>

--- scripts/ci/validate-docs.ts
+++ scripts/ci/validate-docs.ts
@@ -1,4 +1,5 @@
 import execa from 'execa';
+import { setFailed } from '@actions/core';
 
 // This script is meant to only run in CI/GHA.
 // It is used to check whether the documentation is updated or not.
@@ -10,7 +11,8 @@ import execa from 'execa';
   const hasApiDocsChanges = arrayOfDirs.find((dir) => dir.startsWith('api'));
 
   if (hasApiDocsChanges) {
-    throw new Error(
+    // Reference: https://docs.github.com/en/actions/creating-actions/setting-exit-codes-for-actions#setting-a-failure-exit-code-in-a-javascript-action.
+    setFailed(
       "There are API docs changes that aren't committed yet. Please generate them with `yarn generate:docs` then commit it."
     );
   }
