a2b08079c778b1b2df615d0fbf852e346100b896
Try Ajitiono <ballinst@gmail.com>
2021-11-05 10:30:06 +0700

    add comment
    Signed-off-by: Try Ajitiono <ballinst@gmail.com>

--- scripts/ci/validate-docs.ts
+++ scripts/ci/validate-docs.ts
@@ -1,8 +1,17 @@
 import execa from 'execa';
 
+// This script is meant to only run in CI/GHA.
+// It is used to check whether the documentation is updated or not.
 (async () => {
   await execa('yarn', ['generate:docs']);
-  const { stdout } = await execa('git', ['diff --name-only']);
+  const { stdout } = await execa('git', ['diff', '--name-only']);
 
-  console.log(stdout);
+  const arrayOfDirs = stdout.split('\n');
+  const hasApiDocsChanges = arrayOfDirs.find((dir) => dir.startsWith('api'));
+
+  if (hasApiDocsChanges) {
+    throw new Error(
+      "There are API docs changes that aren't committed yet. Please generate them with `yarn generate:docs` then commit it."
+    );
+  }
 })();
